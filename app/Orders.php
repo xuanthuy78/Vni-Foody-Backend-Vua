99350f0aabeb422b7b39970a6f17d61b5ef61be0
vuaho <thivua98@gmail.com>
2020-07-03 11:32:54 +0700

    create model

--- app/Orders.php
+++ app/Orders.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orders extends Model
 {
-    //
+    protected $fillable = [
+        'id', 'name','user_id','status','delivery_address','total_price','note','address','phone','email'
+    ];
+    public function user() {
+        return $this->belongsTo('App\User','user_id','id');
+    }
 }
--- app/Products.php
+++ app/Products.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    protected $fillable = [
+        'id', 'name','code','price','quanlity','description','detail','image','top_hot','categories_id'
+    ];
+    public function categories() {
+        return $this->belongsTo('App\Categories','user_id','id');
+    }
 }
