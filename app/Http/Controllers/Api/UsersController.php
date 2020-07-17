5dbffad42cc5f019d92a0458f12587cf42a31a13
vuaho <thivua98@gmail.com>
2020-07-17 15:39:00 +0700

    Usercontroller

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -80,49 +80,49 @@ class UsersController extends Controller
     
 
     public function changePassword(Request $request, User $id){
-        try {
-            $valid = validator($request->only('old_password', 'new_password', 'confirm_password'), [
-                'old_password' => 'required',
-                'new_password' => 'required|different:old_password',
-                'confirm_password' => 'required_with:new_password|same:new_password|',
-                    ], [
-                'confirm_password.required_with' => 'Confirm password is required.'
-            ]);
+    //     try {
+    //         $valid = validator($request->only('old_password', 'new_password', 'confirm_password'), [
+    //             'old_password' => 'required',
+    //             'new_password' => 'required|different:old_password',
+    //             'confirm_password' => 'required_with:new_password|same:new_password|',
+    //                 ], [
+    //             'confirm_password.required_with' => 'Confirm password is required.'
+    //         ]);
     
-            if ($valid->fails()) {
-                return response()->json([
-                            'errors' => $valid->errors(),
-                            'message' => 'Faild to update password.',
-                            'status' => false
-                                ], 200);
-            }
-    //            Hash::check("param1", "param2")
-    //            param1 - user password that has been entered on the form
-    //            param2 - old password hash stored in database
-            if (Hash::check($request->get('old_password'), Auth::user()->password)) {
-                $user = User::find(Auth::user()->id);
-                $user->password = (new BcryptHasher)->make($request->get('new_password'));
-                if ($user->save()) {
-                    return response()->json([
-                                'data' => [],
-                                'message' => 'Your password has been updated',
-                                'status' => true
-                                    ], 200);
-                }
-            } else {
-                return response()->json([
-                            'errors' => [],
-                            'message' => 'Wrong password entered.',
-                            'status' => false
-                                ], 200);
-            }
-        } catch (Exception $e) {
-            return response()->json([
-                        'errors' => $e->getMessage(),
-                        'message' => 'Please try again',
-                        'status' => false
-                            ], 200);
-        }
+    //         if ($valid->fails()) {
+    //             return response()->json([
+    //                         'errors' => $valid->errors(),
+    //                         'message' => 'Faild to update password.',
+    //                         'status' => false
+    //                             ], 200);
+    //         }
+    // //            Hash::check("param1", "param2")
+    // //            param1 - user password that has been entered on the form
+    // //            param2 - old password hash stored in database
+    //         if (Hash::check($request->get('old_password'), Auth::user()->password)) {
+    //             $user = User::find(Auth::user()->id);
+    //             $user->password = (new BcryptHasher)->make($request->get('new_password'));
+    //             if ($user->save()) {
+    //                 return response()->json([
+    //                             'data' => [],
+    //                             'message' => 'Your password has been updated',
+    //                             'status' => true
+    //                                 ], 200);
+    //             }
+    //         } else {
+    //             return response()->json([
+    //                         'errors' => [],
+    //                         'message' => 'Wrong password entered.',
+    //                         'status' => false
+    //                             ], 200);
+    //         }
+    //     } catch (Exception $e) {
+    //         return response()->json([
+    //                     'errors' => $e->getMessage(),
+    //                     'message' => 'Please try again',
+    //                     'status' => false
+    //                         ], 200);
+    //     }
     }
 
     
